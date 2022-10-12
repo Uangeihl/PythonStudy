ca43221e5b82eb63a0ffc57b37e610f57987cb81
huanglei <1642331764@qq.com>
2022-10-12 14:21:24 +0800

    10.12 commit

---
+++ Leetcode/PlusOne_66.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def plusOne(digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        flag = 1
+        for i in range(len(digits) - 1, -1, -1):
+            if flag == 0:
+                break
+            digits[i] += flag
+            if digits[i] == 10:
+                flag = 1
+                digits[i] = 0
+            else:
+                flag = 0
+        if flag == 1:
+            return [1] + digits
+        else:
+            return digits
+
+
+digits = [9]
+print(Solution.plusOne(digits))
